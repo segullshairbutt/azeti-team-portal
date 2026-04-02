3f1c98ccebee3bbfa1b03200a1f7155c6fa6dd9a
gulshairbutt <segullshairbutt@gmail.com>
2026-04-02 09:53:35 +0200

    Updates the activity entity to use creation timestamp instead of initialization of the date.

--- backend/src/main/java/com/teamportal/model/Activity.java
+++ backend/src/main/java/com/teamportal/model/Activity.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 
@@ -23,7 +24,8 @@ public class Activity {
     private String action;
 
     @Column(nullable = false)
-    private LocalDateTime timestamp = LocalDateTime.now();
+    @CreationTimestamp
+    private LocalDateTime timestamp;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
